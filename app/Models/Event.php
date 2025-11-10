aa87b71c477d5d555fa8af65fb577cfe211e2f00
KOUDAYA Kodjo Joseph <koudayajoseph228@gmail.com>
2025-11-10 16:38:06 +0100

    Mis en place du MVC de la table Event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'vote_amount',
+        'status',
+    ];
+
+   
+    public function candidates()
+    {
+        return $this->hasMany(Candidate::class, 'event_id');
+    }
+   
 }
